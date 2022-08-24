48b4a1f8aa0e6a56cf2b5255c02da990e6c9ed53
PiatekBartosz <bartekpiatek123@gmai.com>
2022-08-24 02:42:35 +0200

    Simple TCP server

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket 
+import threading
+import time
+
+HEADER = 64 # 1st massage to the server size
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) # get host ipaddress
+# SERVER = "192.168.1.203"
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+# this will be running for each thread
+def handle_client(conn, addr):
+    print("f[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) # "blocking"
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept() # wait until new connection, store conn and address
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
